b898259dcb4c3453b762bab4ca839e8884411f58
Yogesh <yogeshwaranrubin@gmail.com>
2020-04-12 13:28:49 +0530

    Created Water particles and calculated its position

--- GeometryCreation/Geometry/Geometry.cpp
+++ GeometryCreation/Geometry/Geometry.cpp
@@ -5,6 +5,7 @@
 
 // Interfaces
 #include "Particle.h"
+#include "Position.h"
 #include "CalculatePosition.h"
 
 // Include dependencies
@@ -30,23 +31,45 @@ int main()
 	}
 	
 	// Create Particle instances
-	vector<Particle*> ListOfAirParticles;
+	vector<Particle*> ListOfAirParticles;	// Air Particles
+	vector<Particle*> ListOfWaterParticles;	// Water Particles
+	int NAirParticles = 0;
+	int NWaterParticles = 0;
 	if (bRc)
 	{
-		int NAirParticles = (gridParams.NX * gridParams.NY); // Total air particles
+		NAirParticles = (gridParams.NX * gridParams.NY); // Total air particles
+		NWaterParticles = NAirParticles;				 // Total Water Particles (currently same as air particles)
 
 		// Create Air Particles		
 		bRc = CreateMultipleParticleInstances(ListOfAirParticles, NAirParticles, INTERFACE_ID::IID_AirParticle);
+
+		// Create Water Particles
+		if (bRc)
+			bRc = CreateMultipleParticleInstances(ListOfWaterParticles, NWaterParticles, INTERFACE_ID::IID_WaterParticle);
 	}
 
 	// Calculate current position of particles	
-	if (bRc)
+	if (bRc && (NAirParticles == ListOfAirParticles.size() && (NWaterParticles == ListOfWaterParticles.size())))
 	{			
 		CalculatePosition* pCalcPosition = NULL;
 		bRc = CreateInstance((void**)&pCalcPosition, INTERFACE_ID::IID_CalculatePosition);
 		if (bRc && pCalcPosition)
 		{
+			// 1. For air particles
 			bRc = pCalcPosition->CalculateCurrentPosition(ListOfAirParticles, gridParams, geoParam2D);
+
+			Position* pStartPosition = NULL;
+			if (bRc)
+			{				
+				Particle* pLastParticle = NULL;
+				pLastParticle = ListOfAirParticles[ListOfAirParticles.size() - 1];
+				if (pLastParticle)									
+					pLastParticle->GetPosition(pStartPosition);							
+			}
+
+			// 2. For water particles
+			if (bRc && pStartPosition)
+				bRc = pCalcPosition->CalculateCurrentPosition(ListOfWaterParticles, gridParams, geoParam2D, pStartPosition);
 		}
 		delete pCalcPosition;
 	}
@@ -55,8 +78,14 @@ int main()
 	
 	// TODO: Create Boundary Particles using ParticleCreation algo.
 
-	// TODO: Print Particle info to a file and view it
-	WriteToFile(ListOfAirParticles, "InitialStateOfParticles");
+	// Writing Initial state of Particle(s) into .csv file (User can view it using "Paraview" application)
+	if (bRc)
+	{
+		vector<Particle*> ListOfAllParticles;
+		ListOfAllParticles.insert(ListOfAllParticles.begin(), ListOfAirParticles.begin(), ListOfAirParticles.end());
+		ListOfAllParticles.insert(ListOfAllParticles.end(), ListOfWaterParticles.begin(), ListOfWaterParticles.end());
+		WriteToFile(ListOfAllParticles, "InitialStateOfParticles");
+	}
 
 	// TODO: Include error library to diplay error codes/ messages
 	return 0;
--- GeometryCreation/InterfaceDLL/CalculatePosition.h
+++ GeometryCreation/InterfaceDLL/CalculatePosition.h
@@ -9,11 +9,12 @@ using namespace std;
 class Particle;
 class GridParams;
 class GeometricParams2D;
+class Position;
 
 class INTERFACEDLLEXPORT CalculatePosition
 {
 public:
-	virtual bool CalculateCurrentPosition(vector<Particle*>& oListOfParticles, const GridParams iGridParams, const GeometricParams2D iGeometricParams2D) = 0;	
+	virtual bool CalculateCurrentPosition(vector<Particle*>& oListOfParticles, const GridParams & iGridParams, const GeometricParams2D & iGeometricParams2D, const Position * ipStartPosition = NULL) = 0;	
 
 	// TODO: Create function for calculating updated position.	
 };
