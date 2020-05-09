4bfc747615e02693208ef0623860e4d72fce23be
Yogesh <yogeshwaranrubin@gmail.com>
2020-05-09 21:33:16 +0530

    Used smart pointer instead of normal pointer to manage lifecycle

--- GeometryCreation/Geometry/Geometry.cpp
+++ GeometryCreation/Geometry/Geometry.cpp
@@ -17,97 +17,97 @@
 using namespace std;
 
 int main()
-{		
-	// 1. Get Geometrical details
-	GeometricParams2D geoParam2D;	
-	GetDataFor2DGeometry(geoParam2D);
-		
-	// 2. Get Grid Params
-	bool bRc = false;
-	GridParams gridParams;
-	if (geoParam2D.GeometricType == GeometricParams2D::GEOMETRIC_TYPE::Rectangle)
-	{		
-		bRc = CalculateGridSize(gridParams, geoParam2D);
-	}
-	
-	// 3. Create Particle instances
-	vector<Particle*> ListOfAirParticles;	
-	vector<Particle*> ListOfWaterParticles;
-	int NAirParticles = 0;
-	int NWaterParticles = 0;
-	if (bRc)
-	{
-		NAirParticles = (gridParams.NX * gridParams.NY); // Total air particles
-		NWaterParticles = NAirParticles;				 // Total Water Particles (currently same as air particles)
+{
+    // 1. Get Geometrical details
+    GeometricParams2D geoParam2D;
+    GetDataFor2DGeometry(geoParam2D);
 
-		// 3.1 Create Air Particles		
-		bRc = CreateMultipleParticleInstances(ListOfAirParticles, NAirParticles, INTERFACE_ID::IID_AirParticle);
+    // 2. Get Grid Params
+    bool bRc = false;
+    GridParams gridParams;
+    if (geoParam2D.GeometricType == GeometricParams2D::GEOMETRIC_TYPE::Rectangle)
+    {
+        bRc = CalculateGridSize(gridParams, geoParam2D);
+    }
 
-		// 3.2 Create Water Particles
-		if (bRc)
-			bRc = CreateMultipleParticleInstances(ListOfWaterParticles, NWaterParticles, INTERFACE_ID::IID_WaterParticle);
-	}
+    // 3. Create Particle instances
+    vector<Particle*> ListOfAirParticles;
+    vector<Particle*> ListOfWaterParticles;
+    int NAirParticles = 0;
+    int NWaterParticles = 0;
+    if (bRc)
+    {
+        NAirParticles = (gridParams.NX * gridParams.NY); // Total air particles
+        NWaterParticles = NAirParticles;				 // Total Water Particles (currently same as air particles)
 
-	// 4. Initial position of particles (at time, t = 0)
-	if (bRc && (NAirParticles == ListOfAirParticles.size() && (NWaterParticles == ListOfWaterParticles.size())))
-	{			
-		CalculatePosition* pCalcPosition = NULL;
-		bRc = CreateInstance((void**)&pCalcPosition, INTERFACE_ID::IID_CalculatePosition);
-		if (bRc && pCalcPosition)
-		{
-			// 4.1: For air particles
-			bRc = pCalcPosition->CalculateCurrentPosition(ListOfAirParticles, gridParams, geoParam2D);
+        // 3.1 Create Air Particles		
+        bRc = CreateMultipleParticleInstances(ListOfAirParticles, NAirParticles, INTERFACE_ID::IID_AirParticle);
 
-			Position* pLastAirParticlePosition = NULL;
-			if (bRc)
-			{				
-				Particle* pLastParticle = NULL;
-				pLastParticle = ListOfAirParticles[ListOfAirParticles.size() - 1];
-				if (pLastParticle)
-				{
-					pLastParticle->AddRef();
-					pLastParticle->GetPosition(pLastAirParticlePosition);					
-				}
-				pLastParticle->Release();
-			}
+        // 3.2 Create Water Particles
+        if (bRc)
+            bRc = CreateMultipleParticleInstances(ListOfWaterParticles, NWaterParticles, INTERFACE_ID::IID_WaterParticle);
+    }
 
-			// 4.2: For water particles
-			if (bRc && pLastAirParticlePosition)
-			{
-				pLastAirParticlePosition->AddRef();
+    // 4. Initial position of particles (at time, t = 0)
+    if (bRc && (NAirParticles == ListOfAirParticles.size() && (NWaterParticles == ListOfWaterParticles.size())))
+    {
+        CalculatePosition* pCalcPosition = NULL;
+        bRc = CreateInstance((void**)&pCalcPosition, INTERFACE_ID::IID_CalculatePosition);
+        if (bRc && pCalcPosition)
+        {
+            // 4.1: For air particles
+            bRc = pCalcPosition->CalculateCurrentPosition(ListOfAirParticles, gridParams, geoParam2D);
 
-				Position* pStartPosition = NULL;
-				bRc = CreateInstance((void**)&pStartPosition, INTERFACE_ID::IID_Position);
-				if (bRc && pStartPosition)
-				{
-					pStartPosition->SetX(pLastAirParticlePosition->GetX() + gridParams.dx);
-					pStartPosition->SetY(0.0);	// To extend only in X-Direction
-					bRc = pCalcPosition->CalculateCurrentPosition(ListOfWaterParticles, gridParams, geoParam2D, pStartPosition);
-				}
-				pStartPosition->Release();
-			}
-			pLastAirParticlePosition->Release();
-		}
-		pCalcPosition->Release();
-	}
-	
-	// TODO: Create Boundary Particles using ParticleCreation algo.
+            Position* pLastAirParticlePosition = NULL;
+            if (bRc)
+            {
+                SmartPtr<Particle> spLastParticle;      // Life-cycle managed automatically
+                spLastParticle = ListOfAirParticles[ListOfAirParticles.size() - 1];
+                if (spLastParticle)
+                {
+                    spLastParticle->GetPosition(pLastAirParticlePosition);
+                    if (pLastAirParticlePosition)
+                        pLastAirParticlePosition->AddRef();
+                }
+            }
 
-	// TODO: Function to Set properties	
+            // 4.2: For water particles
+            if (bRc)
+            {
+                Position* pStartPosition = NULL;
+                bRc = CreateInstance((void**)&pStartPosition, INTERFACE_ID::IID_Position);
+                if (bRc && pStartPosition)
+                {
+                    double currX = 0.0;
+                    if (pLastAirParticlePosition)
+                        currX = pLastAirParticlePosition->GetX();
 
-	// Writing Initial state of Particle(s) into .csv file (User can view it using "Paraview" application)
-	if (bRc)
-	{
-		vector<Particle*> ListOfAllParticles;
-		ListOfAllParticles.insert(ListOfAllParticles.begin(), ListOfAirParticles.begin(), ListOfAirParticles.end());
-		ListOfAllParticles.insert(ListOfAllParticles.end(), ListOfWaterParticles.begin(), ListOfWaterParticles.end());
-		WriteToFile(ListOfAllParticles, "InitialStateOfParticles");
-	}
+                    pStartPosition->SetX(currX + gridParams.dx);
+                    pStartPosition->SetY(0.0);	// To extend only in X-Direction
+                    bRc = pCalcPosition->CalculateCurrentPosition(ListOfWaterParticles, gridParams, geoParam2D, pStartPosition);
+                }
+                pStartPosition->Release();
+            }
+            pLastAirParticlePosition->Release();
+        }
+        pCalcPosition->Release();
+    }
 
-	// TODO: Include error library to diplay error codes/ messages
+    // TODO: Create Boundary Particles using ParticleCreation algo.
 
-	// TODO: Release pointers after using it.	
+    // TODO: Function to Set properties	
 
-	return 0;
+    // Writing Initial state of Particle(s) into .csv file (User can view it using "Paraview" application)
+    if (bRc)
+    {
+        vector<Particle*> ListOfAllParticles;
+        ListOfAllParticles.insert(ListOfAllParticles.begin(), ListOfAirParticles.begin(), ListOfAirParticles.end());
+        ListOfAllParticles.insert(ListOfAllParticles.end(), ListOfWaterParticles.begin(), ListOfWaterParticles.end());
+        WriteToFile(ListOfAllParticles, "InitialStateOfParticles");
+    }
+
+    // TODO: Include error library to diplay error codes/ messages
+        
+    return 0;
 }
 
